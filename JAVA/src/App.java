3c75c723fce2e73f895d1344fbe4fdeaf8119a29
jepetolee <55366212+jepetolee@users.noreply.github.com>
2024-03-20 12:58:16 +0900

    java class

---
+++ JAVA/src/App.java
@@ -0,0 +1,29 @@
+import java.lang.System;
+import java.util.*;
+
+public class App {
+    private static int swap3(int localA, int localB) {
+        return localA;
+    }
+    public static void main(String[] args) {
+       int x= 10;
+       int y= 20;
+       int z =30;
+
+
+       System.out.println(x>=y  ?      x>=z?x:z    :     y>=z?x:z);
+
+    }
+}
+
+/*
+       Scanner ScanningInteger = new Scanner(System.in);
+
+        System.out.println("아무 정수나 넣어보세요");
+
+        int IntegerVariable;
+        IntegerVariable = ScanningInteger.nextInt();
+        if (IntegerVariable %2 ==0)
+            System.out.println("짝수");
+        else
+            System.out.println("짝수"); */
